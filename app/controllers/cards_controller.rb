54f434896ea86cc5e5afbd79e662a3744730e41d
Karine Delvare <kdelvare@gmail.com>
2018-08-11 12:14:12 +0200

    Filter hero cards

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,8 +1,8 @@
 class CardsController < ApplicationController
 	def index
-		@cards = Card.all
+		@cards = Card.where.not(type: 3)  # filter Hero cards
 		@cards = @cards.cardclass(params[:class]) if params[:class].present?
-		@cards = @cards.limit(10)
+		@cards = @cards.limit(14)
 		render json: @cards
 	end
 end
