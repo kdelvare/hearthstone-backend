e8bb41ba0e7a6fc2021f92fa93a0310634da33fd
Karine Delvare <kdelvare@gmail.com>
2018-09-28 08:31:26 +0200

    Stats: cardsets total and owned

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,14 +1,24 @@
 class StatsController < JSONAPI::ResourceController
 	def index
+		@collectible = Card.where.not(type_id: 3).or(Card.where("cost > ?", 0)) # filter basic Hero cards
 		@owned = Card.includes(:collections).where(collections: { user_id: params[:user] }) if params[:user].present?
+		@cardsets = Cardset.where(collectible: true)
+		@rarities = Rarity.where(collectible: true)
 
 		total = {
-			cardsets: [],
-			rarities: [],
-			owned: @owned.count,
-			total: Card.count,
-			rate: 0
+			:cardsets => [],
+			:rarities => [],
+			:owned => @owned.count,
+			:total => @collectible.count
 		}
+
+		@cardsets.each do |cardset|
+			total[:cardsets][cardset.id] = {
+				rarities: [],
+				owned: @owned.where(cardset: cardset.id).count,
+				total: @collectible.where(cardset: cardset.id).count
+			}
+		end
 
 		render json: {
 			data: {
