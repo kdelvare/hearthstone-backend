9be3232bfa30ac08cbe9889d2e418a6d16d9f419
Karine Delvare <kdelvare@gmail.com>
2018-09-28 06:59:05 +0200

    Stats: user filter

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,11 +1,21 @@
 class StatsController < JSONAPI::ResourceController
 	def index
+		@owned = Card.includes(:collections).where(collections: { user_id: params[:user] }) if params[:user].present?
+
+		total = {
+			cardsets: [],
+			rarities: [],
+			owned: @owned.count,
+			total: Card.count,
+			rate: 0
+		}
+
 		render json: {
 			data: {
 				id: 1,
 				type: 'stat',
 				attributes: {
-					total: Card.count
+					total: total
 				}
 			}
 		}
