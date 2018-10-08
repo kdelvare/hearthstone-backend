2094628a9db303ba1fd4c8531fd8fc091cb5ec83
Karine Delvare <kdelvare@gmail.com>
2018-10-08 06:18:08 +0200

    Card: missing filter

--- app/resources/card_resource.rb
+++ app/resources/card_resource.rb
@@ -7,7 +7,7 @@ class CardResource < JSONAPI::Resource
 	belongs_to :rarity
 	has_many :collections
 
-	filters :cardclass, :cost, :rarity, :cardset, :user
+	filters :cardclass, :cost, :rarity, :cardset
 
 	filter :collectible,
 		apply: ->(records, value, _options) {
@@ -24,14 +24,8 @@ class CardResource < JSONAPI::Resource
 			records.joins(:cardset).where(cardsets: { standard: true })
 		}
 
-	class << self
-		def apply_filter(records, filter, value, options)
-			case filter
-				when :user
-					records.joins("LEFT JOIN collections ON collections.card_id = cards.hs_id AND collections.user_id = #{value[0]}")
-				else
-					return super(records, filter, value)
-			end
-		end
-	end
+	filter :missing,
+		apply: ->(records, value, _options) {
+			records.joins("LEFT JOIN collections ON collections.card_id = cards.hs_id AND collections.user_id = #{value.first}").where("(collections.number = 1 AND cards.rarity_id <> 5) OR collections.id IS NULL")
+		}
 end
