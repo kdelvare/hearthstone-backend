a9dc8d468f1dbf698a069cd823496e17b231027e
Karine Delvare <kdelvare@gmail.com>
2018-12-01 19:43:02 +0100

    Wantedcards controller: include user & wanteddeck infos by default

--- app/controllers/wantedcards_controller.rb
+++ app/controllers/wantedcards_controller.rb
@@ -1,2 +1,12 @@
 class WantedcardsController < JSONAPI::ResourceController
+	prepend_before_action :include_user
+
+	def include_user
+		if params[:include] == nil
+			params[:include] = 'user,wanteddeck,wanteddeck.deck,wanteddeck.deck.cardclass,wanteddeck.deck.deckgroup'
+			params[:fields] = {}
+			params[:fields][:wanteddecks] = 'deck'
+			params[:fields][:decks] = 'name,cardclass,deckgroup'
+		end
+	end
 end
