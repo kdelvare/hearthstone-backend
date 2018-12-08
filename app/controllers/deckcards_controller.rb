a2c83b57a51e71c5e92334c6e740628166d9f0b5
Karine Delvare <kdelvare@gmail.com>
2018-12-08 06:41:41 +0100

    Deckcards controller: include deck details by default

--- app/controllers/deckcards_controller.rb
+++ app/controllers/deckcards_controller.rb
@@ -1,2 +1,11 @@
 class DeckcardsController < JSONAPI::ResourceController
+	prepend_before_action :include_deck
+
+	def include_deck
+		if params[:include] == nil
+			params[:include] = 'deck,deck.cardclass,deck.deckgroup'
+			params[:fields] = {}
+			params[:fields][:decks] = 'name,cardclass,deckgroup'
+		end
+	end
 end
