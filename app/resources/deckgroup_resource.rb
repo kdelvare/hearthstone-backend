8a1bca1d06b233823f59b4366bc95b6bf398b7b1
Karine Delvare <kdelvare@gmail.com>
2019-05-05 21:29:04 +0200

    Deckgroup: deck cardclass filter

--- app/resources/deckgroup_resource.rb
+++ app/resources/deckgroup_resource.rb
@@ -5,4 +5,9 @@ class DeckgroupResource < JSONAPI::Resource
 	has_many :decks
 
 	filters :cardset
+
+	filter :cardclass,
+	apply: ->(records, value, _options) {
+		records.includes(:decks).where(decks: { cardclass: value })
+	}
 end
