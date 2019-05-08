74e59e59ec58b6c241f8d61624efdf34dc7792c1
Karine Delvare <kdelvare@gmail.com>
2019-05-08 20:06:35 +0200

    Deckgroup: allow sorting decks by cardclass

--- app/resources/deckgroup_resource.rb
+++ app/resources/deckgroup_resource.rb
@@ -7,7 +7,20 @@ class DeckgroupResource < JSONAPI::Resource
 	filters :cardset
 
 	filter :cardclass,
-	apply: ->(records, value, _options) {
-		records.includes(:decks).where(decks: { cardclass: value })
-	}
+		apply: ->(records, value, _options) {
+			records.includes(:decks).where(decks: { cardclass: value })
+		}
+
+	def self.sortable_fields(context)
+		super + [:"decks.cardclass_id"]
+	end
+
+	def self.apply_sort(records, order_options, context = {})
+		if order_options.has_key?('decks.cardclass_id')
+			records = records.includes(:decks).order('decks.cardclass_id')
+			order_options.delete('decks.cardclass_id')
+		end
+
+		super(records, order_options, context)
+	end
 end
